774dea46a9522b0e6ecccbe932bba631506d57dd
Sw24sX <aleksandr.korolyov.99@mail.ru>
2021-03-04 17:06:42 +0500

    save

--- app/src/main/java/com/example/androidcource/MainActivity.kt
+++ app/src/main/java/com/example/androidcource/MainActivity.kt
@@ -5,22 +5,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = "MyLogs"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.d(TAG, "On create (Main activity)")
         val text = findViewById<TextView>(R.id.mainTextView)
-        text.text = number.toString()
         number++
+        text.text = number.toString()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "On pause (Main activity)")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "On stop (Main activity)")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "On restart (Main activity)")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "On resume (Main activity)")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "On destroy (Main activity)")
     }
 
     companion object {
-        var number: Int = 0
+        private var number: Int = 0
     }
 
     fun onClick(view: View) {
+        Log.d(TAG, "Click button to square")
         val intent: Intent = Intent(this, SquareActivity::class.java)
         intent.putExtra("number", number)
         startActivity(intent)
--- app/src/main/java/com/example/androidcource/SquareActivity.kt
+++ app/src/main/java/com/example/androidcource/SquareActivity.kt
@@ -3,22 +3,51 @@ package com.example.androidcource
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 
 class SquareActivity : AppCompatActivity() {
+    val TAG: String = "MyLogs"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_square)
-
+        Log.d(TAG, "On create (Square activity)")
         val number = intent.extras?.getInt("number")
-        val text = findViewById<TextView>(R.id.text_view)
+        val text = findViewById<TextView>(R.id.squareResult)
         if (number != null) {
             text.text = (number * number).toString()
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "On pause (Square activity)")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "On stop (Square activity)")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "On restart (Square activity)")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "On resume (Square activity)")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "On destroy (Square activity)")
+    }
+
     fun onClick(view: View) {
+        Log.d(TAG, "Click button to main")
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
